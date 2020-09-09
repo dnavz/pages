2f0508a05a5363d1a575a1594df9d14cb4f94742
Epsilon <naveen.dubagunta@live.com>
2020-09-09 13:18:39 +0100

    Distributed Solution

--- src/main/java/org/dell/kube/pages/PageApplication.java
+++ src/main/java/org/dell/kube/pages/PageApplication.java
@@ -2,8 +2,12 @@ package org.dell.kube.pages;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class PageApplication {
 
--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -1,4 +1,6 @@
 package org.dell.kube.pages;
+import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,9 @@ import java.util.List;
 @RequestMapping("/pages")
 public class PageController {
 
+    @Autowired
+    CategoryClient categoryClient;
+
     private IPageRepository pageRepository;
     Logger logger =(Logger)LoggerFactory.getLogger(this.getClass());
 
@@ -23,11 +28,31 @@ public class PageController {
     @PostMapping
     public ResponseEntity<Page> create(@RequestBody Page page) {
 
-        logger.info("CREATE-INFO:Creating page for company = " + page.businessName);
-        logger.debug("CREATE-DEBUG:Creating page for company = " + page.businessName);
+        logger.info("CREATE-INFO:Creating a new page");
+        logger.debug("CREATE-DEBUG:Creating a new  page");
+        Category category = null;
+        try {
+            category = categoryClient.findCategory(page.getCategoryId());
+        }
+        catch(FeignException ex){
+            if(ex.getMessage().contains("404")) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
 
-        Page newPage= pageRepository.create(page);
-        return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+        if(category ==null || category.getId()==null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else
+        {
+            Page newPage = pageRepository.create(page);
+            logger.info("CREATE-INFO:Created a new page with id = " + newPage.id);
+            logger.debug("CREATE-DEBUG:Created a new  page with id = " + newPage.id);
+            return new ResponseEntity<Page>(newPage, HttpStatus.CREATED);
+        }
     }
 
     @GetMapping("{id}")
